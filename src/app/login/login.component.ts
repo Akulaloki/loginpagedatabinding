618cd5c53ab7c8123372ce3eb492c58c262caaae
Akulaloki <141004826+Akulaloki@users.noreply.github.com>
2023-08-08 13:00:30 +0000

    Initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  user: string  = '';
+  password: string = '';
+
+  
+
+
+}
