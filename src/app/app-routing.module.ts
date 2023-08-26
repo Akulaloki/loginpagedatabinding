ee58f352f95f0fa79bdd158afc93a9b78701dcc3
Akulaloki <141004826+Akulaloki@users.noreply.github.com>
2023-08-26 04:59:37 +0000

    update
    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TaskListComponent } from './tasklist/tasklist.component';
+import { TodoComponent } from './todo/todo.component';
 
 
 const routes: Routes = [
   {path:'', redirectTo:'/login',pathMatch: 'full'},
   {path: 'login',component: LoginComponent},
-  {path: 'dashboard',component: DashboardComponent}
+  {path: 'dashboard',component: DashboardComponent},
+  { path: 'todo', component: TodoComponent},
+  { path: 'task', component: TaskListComponent}
 ];
 
 @NgModule({
